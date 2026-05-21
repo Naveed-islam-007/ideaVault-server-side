675847aa820f7905bd0ea4d4be25bd7e914d99db
Naveed-islam-007 <islamnaveed631@gmail.com>
2026-05-22 03:20:07 +0600

    third

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-const express= require('express')
-const cors=require('cors')
-const dotenv=require('dotenv')
+const express = require('express')
+const cors = require('cors')
+const dotenv = require('dotenv')
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 dotenv.config()
 
 const uri = process.env.MONGODB_URI
-const app=express()
-const PORT=process.env.PORT
+const app = express()
+const PORT = process.env.PORT
 app.use(cors())
 app.use(express.json())
 
@@ -20,41 +20,55 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-   const db=client.db('ideavault')
-   const ideascollection=db.collection('ideas')
+    const db = client.db('ideavault')
+    const ideascollection = db.collection('ideas')
 
-   app.post('/ideas', async(req,res)=>{
-    const ideaData=req.body
-    const result=await ideascollection.insertOne(ideaData)
-    res.json(result)
-   })
+    app.post('/ideas', async (req, res) => {
+      const ideaData = req.body
+      const result = await ideascollection.insertOne(ideaData)
+      res.json(result)
+    })
 
-  app.get('/ideas',async(req,res)=>{
-    const result= await ideascollection.find().toArray()
-    res.json(result)
-  })
+    app.get('/ideas', async (req, res) => {
+      const result = await ideascollection.find().toArray()
+      res.json(result)
+    })
 
-  app.get('/ideas/:id',async(req,res)=>{
-    const {id}=req.params
-    const result= await ideascollection.findOne({_id:new ObjectId(id)})
-    res.json(result)
-  })
+    app.get('/ideas/:id', async (req, res) => {
+      const { id } = req.params
+      const result = await ideascollection.findOne({ _id: new ObjectId(id) })
+      res.json(result)
+    })
+
+    app.patch('/ideas/:id', async (req, res) => {
+      const { id } = req.params
+      const updatedData = req.body
+      const result = await ideascollection.updateOne(
+        { _id: new ObjectId(id) },
+        { $set: updatedData }
+      )
+      res.json(result)
+    })
+
+    app.delete('/ideas/:id', async (req, res) => {
+      const { id } = req.params
+      const result = await ideascollection.deleteOne({ _id: new ObjectId(id) })
+      res.json(result)
+    })
 
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
-    // Ensures that the client will close when you finish/error
-  //  await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
 
-app.get('/',(req,res)=>{
-    res.send("Server is running fine")
+app.get('/', (req, res) => {
+  res.send("Server is running fine")
 })
 
-app.listen(PORT,()=>{
-    console.log(`Server is running on port ${PORT}`)
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`)
 })
